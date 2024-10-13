dd351bc15a2f560d4e285887dbf25329df6475f8
Jmartin546 <74067580+Jmartin546@users.noreply.github.com>
2024-10-12 22:44:23 -0400

    Add files via upload
    Final Commit. Couldn't get multiple outputs to textArea to work nor any alphabetizing as wanted. Will continue to work and improve learning lists, dictionaries and text files.

--- bookCatalogFinal.py
+++ bookCatalogFinal.py
@@ -3,23 +3,20 @@ File : bookCatalogFinal.py
 Author : Jeremy Martin
 Last Modified : 10-12-24
 
-The user is prompted to enter a generic temperature and have the program 
-will have two buttons -- one to treat the input as Fahrenheit and convert 
-it to Celsius; and one to treat the input as Celsius and convert it to Fahrenheit.
-
-F = 9/5 * C + 32
-
-F is the Fahrenheit temperature, and C is the Celsius temperature.
+The user is prompted to enter the application to add books to a text area output
+adding the author first and last name as well as the book title. 
 
 """
 
 from breezypythongui import EasyFrame
 
-from tkinter import PhotoImage
+from tkinter import messagebox, PhotoImage
 from tkinter.font import Font
 
 class SplashScreen(EasyFrame):
-    # Displays splash screen
+    # Displays opening screen allowing the user to enter to add books or exit.
+
+    # Initializes the window and widgets with format and text outputs.
     def __init__(self):
         EasyFrame.__init__(self, "Cozy Library Catalog")
         self.setResizable(True)
@@ -38,15 +35,15 @@ class SplashScreen(EasyFrame):
 
         # Add labels and buttons to window
     
-        self.addRecordBtn = self.addButton(text = "Enter to Add Books", row = 5, column = 0, command = self.openAddBooks)
-        self.exitBtn = self.addButton(text = "Exit", row = 5, column = 2, command = lambda:quit())
+        self.addRecordBtn = self.addButton(text = "Enter to Add Books", row = 5, column = 0, command = self.openAddBooks) # Opens second page where the user can enter author/title details.
+        self.exitBtn = self.addButton(text = "Exit", row = 5, column = 2, command = lambda:quit()) # Exit button to close program if no entries are desired.
 
     def openAddBooks(self):
-        self.destroy()
-        AddBooks(self)
+        self.destroy() # Destroys/closes the first window.
+        AddBooks(self) # Opens the second window to add books.
 
 class AddBooks(EasyFrame):
-    """ Demonstrates a multiline text area. """
+    """ Opens the second window where the user can add author/title details."""
     
     def __init__(self, master):
         """ Sets up the window and widgets. """
@@ -56,7 +53,7 @@ class AddBooks(EasyFrame):
         textLabel = self.addLabel(text = "Welcome to the Cozy Library Catalog", row = 0, column = 1, columnspan = 2, sticky = "NSEW")
 
         # Load image and associate it with the image label
-        self.image = PhotoImage(file = "cozyLibrary6.gif")
+        self.image = PhotoImage(file = "cozyLibrary4.gif")
         imageLabel["image"] = self.image
 
         # Set font and color - this is for the caption
@@ -71,33 +68,47 @@ class AddBooks(EasyFrame):
         self.authorFirstName = self.addTextField(text = "", row = 2, column = 2)
         self.authorLastName = self.addTextField(text = "", row = 3, column = 2)
         self.bookTitle = self.addTextField(text = "", row = 4, column = 2)
-        self.outputArea = self.addTextArea("", row = 6, column = 0, columnspan = 3, width = 50, height = 20)
+        self.outputArea = self.addTextArea("", row = 6, column = 0, columnspan = 3, width = 50, height = 5)
 
         self.addBookBtn = self.addButton(text = "Add Book", row = 5, column = 0, command = self.addBook)
         self.exitBtn = self.addButton(text = "Exit", row = 5, column = 2, command = lambda:quit())
-        self.backBtn = self.addButton(text = "Back", row = 5, column = 1, command = self.goBack)
+        self.backBtn = self.addButton(text = "Back", row = 5, column = 1, command = self.goBack) # A button to allow the user to go back to the main page.
 
     def goBack(self):
-        self.destroy()
-        SplashScreen()
+        self.destroy() # Destroys/closes the current window in order to go back to the main page.
+        SplashScreen() # Opens the main window again.
+
 
         
 
     def addBook(self): # Event handling method
-        """Computes the investment schedule base on the inputs and outputs given. """
+        """Takes the user input for the required fields of author first name, author last name and book title."""
         # Obtain and validate the inputs
         authorFirstName = self.authorFirstName.getText()
         authorLastName = self.authorLastName.getText()
         bookTitle = self.bookTitle.getText()
-        if not authorFirstName or not authorLastName or not bookTitle:
-            print("Error")
-            ErrorPrompt()
-        
-"""        
+        if not authorFirstName or not authorLastName or not bookTitle: # Error check to ensure all fields have input, else outputs an error message to let the user know.
+            messagebox.showerror("Input Error", "All fields must be filled.")
+
+        # Set the header for the table
+        result = "%20s %20s %18s \n" % ("Author Last Name", "Author First Name", "Book Title")
+
+        bookInfo = "%20s %20s %18s \n" % (authorLastName, authorFirstName, bookTitle)
+
+        # Output the result while preserving the read-only status
+        self.outputArea["state"] = "normal"
+        self.outputArea.setText(result + "\n" + bookInfo)
+        self.outputArea["state"] = "disabled"
+
+
+"""
+       This section was an early attempt for a prompter box for the error message. I couldn't get it to work properly so found the
+       other method above with the tkinter messagebox function. 
+      
     def errorPrompt(self):
         text = self.prompterBox(title = "Cannot have empty fields.", promptString = "Author First Name")
         self.label["text"] = "Author First Name"
-"""    
+  
 
 
 class ErrorPrompt(EasyFrame):
@@ -106,33 +117,10 @@ class ErrorPrompt(EasyFrame):
         EasyFrame.__init__(self, title = "Error")
         self.addLabel = self.addLabel(text= "Cannot have empty fields.", row = 0, column = 0, sticky = "NSEW")
         self.addButton(text = "Return", row = 1, column = 0, command = AddBooks())
-
-"""
-
-        
-        
-        
+ 
         # Set the header for the table
-        result = "%4s %18s %10s %16s \n" % ("Author Last Name", "Author First Name", "Book Title")
-        
-        # Compute and append the results for each year
-        totalInterest = 0.0
-        for year in range(1, years + 1):
-            interest = startBalance * rate
-            endBalance = startBalance + interest
-            result += "%4d %18.2f %10.2f %16.2f \n" % (year, startBalance, interest, endBalance)
-            startBalance = endBalance
-            totalInterest += interest
-
-        # Append the totals for the period
-        result += "Ending Balance: $%0.2f \n" % endBalance
-        result += "Total interest earned: $%0.2f \n" % totalInterest
-
-        # Output the result while preserving the read-only status
-        self.outputArea["state"] = "normal"
-        self.outputArea.setText(result)
-        self.outputArea["state"] = "disabled"
-        """
+    result = "%18s %18s %18s \n" % ("Author Last Name", "Author First Name", "Book Title")
+"""
         
         
 def main():
